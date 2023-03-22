1c795bf10957d79e4770fce915ab0803444203f9
Ben Li <ben10723021@gmail.com>
2023-03-22 23:30:59 +0800

    init: project init - vue-i18n

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import '@/assets/tailwindcss/index.scss';
 import { createApp } from 'vue';
 import App from './App.vue';
 
+import { i18n } from '@/composables/useI18n';
 import router from './router';
 import store from './store';
 
-const app = createApp(App).use(store);
+const app = createApp(App);
+app.use(store);
+app.use(i18n);
 app.use(router).mount('#app');
